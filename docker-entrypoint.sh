806bc93d3d0e54b3372457fa93120eeaa316f374
Sean Bradford <bradfordphonic@gmail.com>
2023-07-19 17:12:51 -0500

    Deleted webpacker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
-rails webpacker:install
-rails webpacker:install:react
+
+bundle exec vite install
 
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 
