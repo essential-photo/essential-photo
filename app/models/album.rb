0e83b98690c70616c5bbb64f920be84deaa314db
Stephen Allard <stephencallard@gmail.com>
2023-09-03 16:33:47 -0500

    add associations between Image and Album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
 class Album < ActiveRecord::Base
+    has_many :images, dependent: :destroy
+    has_many :albums, dependent: :destroy
+    belongs_to :album, optional: true
 end
     
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,7 @@ class Image < ApplicationRecord
   has_one_attached :thumbnail
   has_many :image_tags, dependent: :destroy
   has_many :tags, through: :image_tags
+  belongs_to :album, optional: true
 
   validates :image, attached: true, content_type: [:png, :jpg, :jpeg]
   validates :is_public, inclusion: [true, false]
