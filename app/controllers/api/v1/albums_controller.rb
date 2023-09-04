8e4d8659927031c6a97ce3f473c3df7dd1ee83f2
Stephen Allard <stephencallard@gmail.com>
2023-09-04 12:16:45 -0500

    add albums create endpoint

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,15 +1,35 @@
 class Api::V1::AlbumsController < ApplicationController
+    before_action :authenticate_api_v1_admin!, except: :index
+
     def index
         @albums = Album.all
 
-        formatted_albums = @albums.map{|album|
-            {
-                id: album.id,
-                name: album.name,
-                parent_album_id: album.album_id
-            }
-        }
+        formatted_albums = @albums.map{|album| formatted_album(album)}
 
         render json: formatted_albums, status: :ok
+    end
+
+    def create
+        @album = Album.new(album_params)
+
+        if @album.save
+            render json: formatted_album(@album), status: :created
+        else
+            render json: @album.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def album_params 
+        params.require(:album).permit(:name, :album_id)
+    end
+
+    def formatted_album(album)
+        return {
+            id: album.id,
+            name: album.name,
+            parent_album_id: album.album_id
+        }
     end
 end
