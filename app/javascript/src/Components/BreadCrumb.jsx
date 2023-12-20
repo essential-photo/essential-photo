857071b803fedddde031cf94a7e3fae54ee3f0c7
Stephen Allard <stephencallard@gmail.com>
2023-12-19 20:09:36 -0600

    only render the last 2 breadcrumbs

--- app/javascript/src/Components/BreadCrumb.jsx
+++ app/javascript/src/Components/BreadCrumb.jsx
@@ -2,26 +2,64 @@ import React from 'react';
 import './BreadCrumb.css';
 import houseIcon from '../images/house-icon.svg';
 import greaterThanArrowIcon from '../images/greater-than-arrow-icon.svg';
+import dotsIcon from '../images/dots-icon.svg';
 
 export default function BreadCrumb(props) {
-  const crumbs = props.getAlbumPath(props.selectedAlbumId).map(waypoint => {
-    return (
+  let crumbs = [];
+  const albumPath = props.getAlbumPath(props.selectedAlbumId);
+  
+  // Display the album path as breadcrumbs.
+  // However, if we are more than 2 albums deep, render only the last 
+  // 2 albums in the breadcrumbs, with '...' in the front
+  if (albumPath.length <= 2) {
+    crumbs = albumPath.map(waypoint => {
+      return (
+        <>
+          <img
+            src={greaterThanArrowIcon}
+            className="breadCrumb__rightIcon"
+            alt="this is a right arrow icon"
+          ></img>
+          <span
+            className="breadCrumb__crumb"
+            onClick={() => {handleClick(waypoint.id)}}
+          >
+            {waypoint.name}
+          </span>
+        </>
+      )
+    });
+  } else {
+    crumbs.push(
       <>
         <img
-          src={greaterThanArrowIcon}
-          className="breadCrumb__rightIcon"
-          alt="this is a right arrow icon"
+          src={dotsIcon}
+          className="breadCrumb__dotsIcon"
+          alt="this is a dots icon"
         ></img>
-        <span
-          className="breadCrumb__crumb"
-          onClick={() => {handleClick(waypoint.id)}}
-        >
-          {waypoint.name}
-        </span>
       </>
     )
-  });
 
+    // add the last 2 albums
+    crumbs.push(albumPath.slice(albumPath.length - 2, albumPath.length).map(waypoint => {
+      return (
+        <>
+          <img
+            src={greaterThanArrowIcon}
+            className="breadCrumb__rightIcon"
+            alt="this is a right arrow icon"
+          ></img>
+          <span
+            className="breadCrumb__crumb"
+            onClick={() => {handleClick(waypoint.id)}}
+          >
+            {waypoint.name}
+          </span>
+        </>
+      )
+    }));
+  }
+  
   function handleClick(albumId) {
     // only refresh if the clicked on album is different
     // from what's currently displayed
@@ -31,6 +69,8 @@ export default function BreadCrumb(props) {
       props.setSelectedAlbumId(albumId);
     }
   }
+
+  console.log(props.getAlbumPath(props.selectedAlbumId))
 
   return (
     <div className="breadCrumb">
